5932a91d21151a6c3850bdec4fbdc35c2d8da30a
LukitaNX <gmrecolletion@gmail.com>
2022-01-31 17:10:09 -0300

    Initial Release v0.0.1b

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+document.querySelector('.urlinsta').addEventListener('mouseover', () => {
+document.querySelector('.instaicon').classList.toggle('showiconig');
+});
+document.querySelector('.urltwitter').addEventListener('mouseover', () => {
+document.querySelector('.twittericon').classList.toggle('showicontr');
+});
+document.querySelector('.urlyoutube').addEventListener('mouseover', () => {
+document.querySelector('.youtubeicon').classList.toggle('showiconyt');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cripto-post', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
